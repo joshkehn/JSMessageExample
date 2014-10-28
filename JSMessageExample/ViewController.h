86f05a43cc12f1f69fe8fc0feee2b36adc8a3aea
Joshua Kehn <josh.kehn@gmail.com>
2014-10-27 23:02:15 -0400

    Initial Commit

---
+++ JSMessageExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSMessageExample
+//
+//  Created by Joshua Kehn on 10/27/14.
+//  Copyright (c) 2014 KEHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSMessageExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSMessageExample
+//
+//  Created by Joshua Kehn on 10/27/14.
+//  Copyright (c) 2014 KEHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
